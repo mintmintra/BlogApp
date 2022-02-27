ce97e6f196b78f0ac7c652490d587010f6b9c368
mintmintra <mintracss019@gmail.com>
2022-02-28 02:30:54 +0700

    validator

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var { check, validationResult } = require('express-validator');
+
 //Connect DB
 var mongodb = require('mongodb');
 var db = require('monk')('localhost:27017/BlogDB')
@@ -18,8 +20,28 @@ router.get('/category/add', function(req, res, next) {
   res.render('addcategory');
 });
 
-router.post('/category/add', function(req, res, next) {
-  res.render('addcategory');
+router.post('/category/add', [
+  check('name','ชื่อประเภทต้องไม่เป็นค่าว่าง').not().isEmpty()
+], function(req, res, next) {
+  var result = validationResult(req);
+  var errors = result.errors;
+  if (!result.isEmpty()) {
+    res.render('addcategory', { errors: errors });
+  } else {
+    //บันทึกข้อมูลประเภท
+    var category = db.get('categories')
+    category.insert({
+      name:req.body.name
+    }, function (err, success) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.location('/');
+        res.redirect('/');
+      }
+    })
+  }
+  
 });
 
 module.exports = router;
