fd3bcbd311902d954900efb88b52787c8d9b385a
mintmintra <mintracss019@gmail.com>
2022-03-03 23:47:14 +0700

    validation

--- routes/index.js
+++ routes/index.js
@@ -10,15 +10,49 @@ var db = require('monk')('localhost:27017/BlogDB')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var blogs = db.get('posts')
+  var categories = db.get('categories')
   blogs.find({}, {}, function (err, blog) {
-    res.render('index', { posts: blog})
-  })
+    categories.find({}, {}, function (err, category) {
+    res.render('index', {
+      posts: blog, categories:category
+    });
+  });
+  });
+  
 });
+
 
 
 router.get('/category/add', function(req, res, next) {
   res.render('addcategory');
 });
+
+router.get('/post/add', function (req, res, next) {
+  var categories = db.get('categories')
+  categories.find({}, {}, function (err, category) {
+    res.render('addpost', {
+      categories:category
+    });
+   });
+});
+
+router.post('/post/add', [
+  check('title', 'กรุณาป้อนชื่อบทความ').not().isEmpty(),
+  check('content', 'กรุณาป้อนเนื้อหา').not().isEmpty(),
+  check('img', 'กรุณาใส่ภาพปก').not().isEmpty(),
+  check('author', 'กรุณาป้อนชื่อผู้เขียน').not().isEmpty()
+], function (req, res, next) {
+  var result = validationResult(req);
+  var errors = result.errors;
+  if (!result.isEmpty()) {
+    res.render('addpost', {
+      errors: errors
+    });
+  } else {
+
+  }
+});
+
 
 router.post('/category/add', [
   check('name','ชื่อประเภทต้องไม่เป็นค่าว่าง').not().isEmpty()
